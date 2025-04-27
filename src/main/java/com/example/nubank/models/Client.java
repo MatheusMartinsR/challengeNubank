c7d0528595a3a3f88d79a6f9ffcba7e96bacd050
Matheus Rocha <matheus.rocha@scicrop.com>
2025-04-26 22:56:41 -0300

    feat: starting project

---
+++ src/main/java/com/example/nubank/models/Client.java
@@ -0,0 +1,30 @@
+package com.example.nubank.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "client")
+@Data
+public class Client {
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "password")
+  private String password;
+
+}
