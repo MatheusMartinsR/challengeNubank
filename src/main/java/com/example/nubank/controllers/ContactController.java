bec0f09fc417e6df46f5ee56bd147383dd470432
Matheus Rocha <matheus.rocha@scicrop.com>
2025-05-01 13:39:12 -0300

    fix: removing some imports that i am not usign now

--- src/main/java/com/example/nubank/controllers/ContactController.java
+++ src/main/java/com/example/nubank/controllers/ContactController.java
@@ -3,20 +3,12 @@ package com.example.nubank.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.nubank.dto.ContactDTO;
 import com.example.nubank.models.Contact;
 import com.example.nubank.repository.ContactRepository;
-import com.example.nubank.services.ContactService;
 
 @RestController
 @RequestMapping("/contacts")
@@ -24,9 +16,6 @@ public class ContactController {
 
   @Autowired
   private ContactRepository contactRepository;
-
-  @Autowired
-  private ContactService contactService;
 
   @GetMapping
   public List<Contact> getAllContacts() {
